b64186fa8a1c567202765277484bfdd921f6a08a
Yossi Eynav <yossi.eynav@gmail.com>
2017-04-20 18:48:11 +0300

    [REFACTOR] - Remove webpack uglify plugin

--- webpack/webpack.config.production.js
+++ webpack/webpack.config.production.js
@@ -17,7 +17,6 @@ module.exports = {
     },
         plugins:[
             new webpack.optimize.OccurrenceOrderPlugin(),
-            new webpack.optimize.UglifyJsPlugin(),
             new webpack.optimize.DedupePlugin(),
             new CopyWebpackPlugin([{
                 from: './assets', to: distPath
