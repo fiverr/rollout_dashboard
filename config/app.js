ae32a1fd3e11faf0212ddf7009ca4254aa126dcd
Artem Vasiliev <artem.job@gmail.com>
2017-07-09 14:46:07 +0300

    chore: simplify config for Heroku deployment

--- config/app.js
+++ config/app.js
@@ -3,15 +3,15 @@ const rc = require('rc');
 
 const config =  {
   development: {
-    rolloutServiceHost: 'http://localhost',
-    rolloutServicePort: '9999',
-    port: 1234,
+    rolloutServiceHost: process.env['SERVICE_HOST'] || 'http://localhost',
+    rolloutServicePort: process.env['SERVICE_PORT'] || '9999',
+    port: process.env['PORT'] || 1234,
     googleAuth: rc('gauth', {})
   },
   production: {
-    rolloutServiceHost: '{{ key "configurations/microservices/rollout/rollout-service-url" }}',
-    rolloutServicePort: '{{ key "configurations/microservices/rollout/rollout-service-port" }}',
-    port: '{{ key "configurations/microservices/rollout/rollout-dashboard-port" }}',
+    rolloutServiceHost: process.env['SERVICE_HOST'],
+    rolloutServicePort: process.env['SERVICE_PORT'] || 443,
+    port: process.env['PORT'] || '{{ key "configurations/microservices/rollout/rollout-dashboard-port" }}',
     googleAuth: {
       apiKey: '{{ key "configurations/microservices/rollout/google-auth-api-key" }}',
       clientId: '{{ key "configurations/microservices/rollout/google-auth-client-id" }}'
