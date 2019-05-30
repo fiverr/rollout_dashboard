659022658baf3f58ffa6baf01e447d77226b397a
Tom Mizrachi <tom.mizrachi@fiverr.com>
2019-05-30 16:38:11 +0300

    fork this repo to a private one

---
+++ src/components/Styles/PredefinedStyles.tsx
@@ -0,0 +1,30 @@
+import {black500, blue500, blue800} from 'material-ui/styles/colors';
+
+const PredefinedStyles = {
+  errorStyle: {
+    color: black500,
+  },
+  underlineStyle: {
+    borderColor: black500,
+  },
+  floatingLabelStyle: {
+    color: black500,
+  },
+  floatingLabelFocusStyle: {
+    color: blue500,
+  },
+  dialogTitleStyle: {
+    backgroundColor: blue800,
+    color: 'white',
+    textAlign: 'center',
+  },
+  customWidth: {
+    width: 300,
+  },
+  toggle: {
+    marginTop: 20,
+    marginBottom: 20,
+  }
+};
+
+export default PredefinedStyles
